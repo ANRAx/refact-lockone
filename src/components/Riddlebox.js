46c6563fdae5eaab3ccb184551aa843717e5ab12
Sadhguru <ramdassaj@gmail.com>
2020-04-23 16:27:35 -0400

    update border

--- src/components/Riddlebox.js
+++ src/components/Riddlebox.js
@@ -8,7 +8,7 @@ const Riddlebox = () => {
                 <h1 className="f4 bg-light-green br3 br--top black-60 mv0 pv2 ph3 tc">Challenge 1</h1>
                 <div className="pa3 bt b--black-10 bg-near-white">
                     <p className="f6 f5-ns lh-copy measure">
-                    Read the hints below and click <a href="www.google.com">here</a> to work through your first challenge!
+                    Read the hints below and click <a href="https://www.google.com">here</a> to work through your first challenge!
                     </p>
                     <ol className="f6 f5-ns lh-copy measure">
                         <li>Hint 1</li>
@@ -19,10 +19,10 @@ const Riddlebox = () => {
             </article>
             <article className="pa3 pa5-ns">
                 <ul className="list pl0 ml0 center mw4 ba b--white br2 bg-white">
-                    <li className="tc mv1 pv3 bb b--black grow bg-green">Lock One</li>
-                    <li className="tc mv1 pv3 bb b--black grow bg-yellow">Lock Two</li>
-                    <li className="tc mv1 pv3 bb b--black grow bg-orange">Lock Three</li>
-                    <li className="tc mv1 pv3 bb b--black grow bg-red">Lock Four</li>
+                    <li className="tc mv1 pv3 ba b--black grow pointer bg-green">Lock One</li>
+                    <li className="tc mv1 pv3 ba b--black grow pointer bg-yellow">Lock Two</li>
+                    <li className="tc mv1 pv3 ba b--black grow pointer bg-orange">Lock Three</li>
+                    <li className="tc mv1 pv3 ba b--black grow pointer bg-red">Lock Four</li>
                 </ul>
             </article>
         </div>
